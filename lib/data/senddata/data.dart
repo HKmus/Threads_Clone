09b5a6d37e873b8fcf179b931cc2f0fc052a7eba
HKmus <mus.kch03@gmail.com>
2023-07-26 00:32:19 +0100

    clone

---
+++ lib/data/senddata/data.dart
@@ -0,0 +1,41 @@
+List listThear() {
+  return [
+    {
+      'name': 'Mustapha',
+      'posted': '9 m',
+      'description': 'Hi threads',
+      'image': '',
+      'profileImage': 'profile.jpg',
+      'replies': '120',
+      'liks': '198',
+      'replier': 'h2.jpeg',
+      'replier1': 'p2.jpg',
+      'replier2': 'p3.jpg',
+    },
+    {
+      'name': 'said',
+      'posted': '30 m',
+      'description': 'to day',
+      'image': 'p3.jpg',
+      'profileImage': 'p3.jpg',
+      'replies': '10',
+      'liks': '18',
+      'replier': 'h2.jpeg',
+      'replier1': 'p2.jpg',
+      'replier2': 'profile.jpg',
+    },
+    {
+      'name': 'houari',
+      'posted': '40 m',
+      'description':
+          "Flutter is an open source framework developed and supported by Google. Frontend and full-stack developers use Flutter to build an application’s user interface (UI) for multiple platforms with a single codebase.",
+      'image': '',
+      'profileImage': 'h2.jpeg',
+      'replies': '40',
+      'liks': '198',
+      'replier': 'h2.jpeg',
+      'replier1': 'p2.jpg',
+      'replier2': 'p3.jpg',
+    }
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'login_page.dart';
+import 'package:threads/widgets/navigartion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,20 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent, // status bar color
-      ),
-    );
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true),
-      home: const LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: Navi(),
     );
   }
 }
